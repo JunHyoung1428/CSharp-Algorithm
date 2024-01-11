ace78bf2f41705a45e8e1b95e9fe55c06ed3a355
Park Jun Hung <matte1428@naver.com>
2024-01-11 15:45:59 +0900

    feat:Count,IndexOf

--- 01.List/Task2.cs
+++ 01.List/Task2.cs
@@ -25,11 +25,24 @@ namespace _01.List
 
             public void GetItem(Item item)
             {
+                Console.WriteLine($"인벤토리에 {item.name}을 추가합니다.");
                 items.Add(item);
             }
             public void DropItem(Item item)
             {
+                Console.WriteLine($"인벤토리에서 {item.name}을 버립니다.");
                 items.Remove(item);
+            }
+
+            public int ItemCount()
+            {
+                Console.WriteLine($"인벤토리의 아이템 갯수:{items.Count}");
+                return items.Count;
+            }
+
+            public int FindItem(Item item)
+            {
+               return items.IndexOf(item);
             }
         }
 
