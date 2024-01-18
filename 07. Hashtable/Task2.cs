50cde3ae32fd5ae1fa49300f755eb437b1ad3435
Park Jun Hyoung <matte1428@naver.com>
2024-01-18 17:41:37 +0900

    docs

--- 07. Hashtable/Task2.cs
+++ 07. Hashtable/Task2.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-/*
+/* <백준 17219>
  * 첫째 줄에 저장된 사이트 주소의 수 N(1 ≤ N ≤ 100,000)과 비밀번호를 찾으려는 사이트 주소의 수 M(1 ≤ M ≤ 100,000)이 주어진다.
 두번째 줄부터 N개의 줄에 걸쳐 각 줄에 사이트 주소와 비밀번호가 공백으로 구분되어 주어진다.
 사이트 주소는 알파벳 소문자, 알파벳 대문자, 대시('-'), 마침표('.')로 이루어져 있고, 중복되지 않는다.
@@ -15,7 +15,7 @@ namespace _07._Hashtable
 {
     internal class Task2
     {
-        static void Main()
+        static void Main2()
         {
             Dictionary<string, string> dict = new Dictionary<string, string>();
             string[] input = new string[2];
