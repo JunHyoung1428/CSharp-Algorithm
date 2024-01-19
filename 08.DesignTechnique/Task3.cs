5f834435532dcbf3c42fef3001aac71dd5ccfbe4
Park Jun Hyoung <matte1428@naver.com>
2024-01-19 14:23:25 +0900

    docs

---
+++ 08.DesignTechnique/Task3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 백준 2630 색종이 만들기  https://www.acmicpc.net/problem/2630
+ * 입력
+첫째 줄에는 전체 종이의 한 변의 길이 N이 주어져 있다. N은 2, 4, 8, 16, 32, 64, 128 중 하나이다.
+색종이의 각 가로줄의 정사각형칸들의 색이 윗줄부터 차례로 둘째 줄부터 마지막 줄까지 주어진다.
+하얀색으로 칠해진 칸은 0, 파란색으로 칠해진 칸은 1로 주어지며, 각 숫자 사이에는 빈칸이 하나씩 있다.
+
+출력
+첫째 줄에는 잘라진 햐얀색 색종이의 개수를 출력하고, 둘째 줄에는 파란색 색종이의 개수를 출력한다.
+
+ */
+namespace _08.DesignTechnique
+{
+    internal class Task3
+    {
+    }
+}
