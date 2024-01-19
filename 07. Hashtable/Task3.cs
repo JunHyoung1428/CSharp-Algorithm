1e82fbaca8de57d6e9ce6ac77a32f0ee56bc4a78
Park Jun Hyoung <matte1428@naver.com>
2024-01-19 10:40:11 +0900

    docs

--- 07. Hashtable/Task3.cs
+++ 07. Hashtable/Task3.cs
@@ -6,6 +6,9 @@
 첫째 줄에 만들 수 있는 정수의 개수를 출력한다.
  */
 
+
+
+
 using System.Text;
 
 namespace _07._Hashtable
@@ -34,7 +37,62 @@ namespace _07._Hashtable
             // HashSet 요소 갯수 출력
             Console.WriteLine(set.Count);
         }
-
-
     }
 }
+
+
+/*
+ * using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static int n, k;
+    static int[] cards;
+    static bool[] visited;
+    static HashSet<string> integers;
+
+    static void Main(string[] args)
+    {
+        // n, k 입력 받기
+        string[] nk = Console.ReadLine().Split();
+        n = int.Parse(nk[0]);
+        k = int.Parse(nk[1]);
+
+        // 카드에 적힌 수 입력 받기
+        cards = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            cards[i] = int.Parse(Console.ReadLine());
+        }
+
+        visited = new bool[n];
+        integers = new HashSet<string>();
+
+        // 순열을 사용하여 가능한 정수의 개수 계산
+        Permutation(0, "");
+
+        // 결과 출력
+        Console.WriteLine(integers.Count);
+    }
+
+    static void Permutation(int count, string integer)
+    {
+        if (count == k)
+        {
+            integers.Add(integer);
+            return;
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            if (!visited[i])
+            {
+                visited[i] = true;
+                Permutation(count + 1, integer + cards[i]);
+                visited[i] = false;
+            }
+        }
+    }
+}
+ */
